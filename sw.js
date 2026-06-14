e2eeb77209d10fb9b4eb31ed1fc577b63ba548b3
BlokNet <bloknett@gmail.com>
2026-06-14 20:53:16 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 // свежие файлы с сервера.
 // ============================================================
 
-const CACHE_VERSION = 'kipia-v34';
+const CACHE_VERSION = 'kipia-v35';
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
